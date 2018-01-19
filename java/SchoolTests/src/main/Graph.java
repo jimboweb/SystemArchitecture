b16eddb87b3b443361d98bec1e68b3625f94460b
Julien Olsson <julienolsson@Juliens-MacBook-Air.local>
2018-01-19 10:36:21 -0500

    added Hashing algorithm
    Wahoo its a me

---
+++ java/SchoolTests/src/main/Graph.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Graph {
+	GraphEdge[] edges;
+	public Graph() {
+		
+	}
+}
---
+++ java/SchoolTests/src/main/GraphEdge.java
@@ -0,0 +1,12 @@
+package main;
+
+public class GraphEdge {
+	int a;
+	int b;
+	int dist;
+	public GraphEdge(int a_, int b_, int dist_) {
+		a = a_;
+		b = b_;
+		dist = dist_;
+	}
+}
